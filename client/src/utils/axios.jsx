61a46f3b3a16fa92f85cc21a68e1dd1a08c95bea
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2024-08-22 10:34:50 +0300

    api url from render

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8888/", // api url
-  //   baseURL: "https://evangadi-forum-server-nzfs.onrender.com", // api url
+  // baseURL: "http://localhost:8888/", // api url
+  baseURL: "https://prodigy-fs-01-1.onrender.com", // api url
 
   withCredentials: true,
 });
